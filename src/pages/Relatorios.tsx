a274096d15360f4ca2af1182fb4ad3e387e3bb19
N1nji <pedro.felipe7911@gmail.com>
2025-12-21 17:52:19 -0300

    Testando new interface

--- src/pages/Relatorios.tsx
+++ src/pages/Relatorios.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useCallback } from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
