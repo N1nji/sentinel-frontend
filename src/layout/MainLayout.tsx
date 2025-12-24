9908681292dab2731fdaea0c11094c3ac42ca1b7
N1nji <pedro.felipe7911@gmail.com>
2025-12-24 00:48:58 -0300

    Fix BUGS

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -5,16 +5,28 @@ import FloatingAIButton from "../components/FloatingAIButton";
 
 export default function MainLayout() {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
+    // "flex-col lg:flex-row" faz ficar um embaixo do outro no celular e lado a lado no PC
+    <div className="flex flex-col lg:flex-row h-screen bg-gray-50 overflow-hidden">
+      
+      {/* SIDEBAR: No celular ela precisa de um tratamento especial (ou sumir) */}
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 flex flex-col min-w-0 h-screen">
         <Header />
-        <main className="p-6 bg-gray-100 min-h-0 overflow-auto">
-          <Outlet />
+        
+        {/* CONTEÚDO PRINCIPAL */}
+        <main className="flex-1 p-4 sm:p-6 overflow-auto custom-scrollbar">
+          <div className="max-w-[1600px] mx-auto w-full">
+            <Outlet />
+          </div>
         </main>
 
-        {/* Botão global da IA */}
-        <FloatingAIButton />
+        {/* Botão global da IA - Garante que ele não tampe nada importante no mobile */}
+        <div className="fixed bottom-4 right-4 z-50 lg:bottom-8 lg:right-8">
+          <FloatingAIButton />
+        </div>
       </div>
     </div>
   );
